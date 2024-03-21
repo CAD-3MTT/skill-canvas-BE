f47926a28bc6ed998a37e68588effb9c64ccc56c
henrynoble01 <henrynoble01@gmail.com>
2024-03-21 18:01:57 +0100

    chore: rectify

--- src/mail.js
+++ src/mail.js
@@ -3,9 +3,7 @@ import postmark from "postmark";
 export function sendMail(req, res) {
   // Send an email:
   //   var client = new postmark.ServerClient("POSTMARK-SERVER-API-TOKEN-HERE");
-  var client = new postmark.ServerClient(
-    "e0d8ceeb-97ab-492c-8527-dcb5b20dd68d"
-  );
+  var client = new postmark.ServerClient();
 
   client.sendEmail({
     // From: "sender@example.com",
